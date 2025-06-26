e2165ab60c928facbb530ead9ea0ba2448aa79f2
raz <pro@raz.tw>
2025-06-26 23:45:38 +0800

    v0.1.5

--- src/conf.py
+++ src/conf.py
@@ -247,7 +247,7 @@ def pathFromRoot(path):
 # envs
 #------------------------------------------------------------------------
 class envs:
-    version='0.1.4'
+    version='0.1.5'
     isDev = False if isDock else bool(os.getenv('IsDev', False))
     isDevUI = False if isDock else bool(os.getenv('IsDevUI', False))
     isDock = False if not isDock else True
